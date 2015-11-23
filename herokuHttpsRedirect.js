c96ca7d8765da2fbaa795fe694b2772608f79532
Martin Gammelsæter <martin.gammelsaeter@blankoslo.no>
2015-11-23 11:02:17 +0100

    Remove unnecessary code.

--- herokuHttpsRedirect.js
+++ herokuHttpsRedirect.js
@@ -2,9 +2,6 @@ module.exports = function(req, res, next) {
     if (process.env.NODE_ENV === 'production') {
         if (req.headers['x-forwarded-proto'] != 'https') {
             return res.redirect('https://' + req.headers.host + req.url);
-        } else {
-            next();
-            return;
         }
     }
 
